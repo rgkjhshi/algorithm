0da575a2c0fecae120289df677f33b83dbdd152a
Michael King <rgkjhshi@gmail.com>
2018-07-06 17:59:17 +0800

    test

--- src/test/java/com/mk/algorithm/UnitTest.java
+++ src/test/java/com/mk/algorithm/UnitTest.java
@@ -1,10 +1,13 @@
 package com.mk.algorithm;
 
-import org.joda.time.*;
-import org.joda.time.format.DateTimeFormat;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 /**
  * 单元测试基础类
@@ -23,39 +26,13 @@ public class UnitTest {
     @Test
     public void testDate() {
         // 夏令时日期
-        LocalDate date = LocalDate.parse("19880410", DateTimeFormat.forPattern("yyyyMMdd"));
-        logger.info(date.toString("yyyy-MM-dd"));
-        int age = Years.yearsBetween(date, LocalDate.now()).getYears();
-        logger.info("age={}", age);
+        LocalDate date0 = LocalDate.parse("19880410", DateTimeFormatter.ofPattern("yyyyMMdd"));
+        LocalDate date = LocalDate.parse("1988-04-10");
+        logger.info(date.toString());
+        long years = date.until(LocalDate.now(), ChronoUnit.YEARS);
+        logger.info("age={}", years);
         LocalTime time = LocalTime.parse("00:30:00");
-        logger.info(time.toString("HH:mm:ss"));
-    }
-
-    @Test
-    public void createTable() {
-        String str = "CREATE TABLE `character_data_%04d` LIKE `character_data_3000`;";
-        for (int i = 3001; i < 4000; i++) {
-            System.out.println(String.format(str, i));
-        }
-    }
-
-    @Test
-    public void alterTable() {
-        String s2 = "drop table `character_data_%03d`;";
-//        String str = "alter table `character_data_%03d` modify `character_value` LONGTEXT COMMENT '指标值';";
-        for (int i = 0; i < 1000; i++) {
-            System.out.println(String.format(s2, i));
-//            System.out.println(String.format(str, i));
-        }
-    }
-
-    @Test
-    public void testTime() {
-        String pattern = "yyyy-MM-dd HH:mm:ss";
-        DateTime start = DateTime.parse("2018-02-11 01:00:00", DateTimeFormat.forPattern(pattern));
-        DateTime end = DateTime.parse("2018-02-12 01:00:00", DateTimeFormat.forPattern(pattern));
-        logger.info("" + Minutes.minutesBetween(DateTime.now(), start).getMinutes());
-        logger.info("" + Minutes.minutesBetween(DateTime.now(), end).getMinutes());
+        logger.info(time.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
     }
 
 }
