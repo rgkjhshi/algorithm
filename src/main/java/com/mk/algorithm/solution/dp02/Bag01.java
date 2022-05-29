6e1cd14a4304decf15d56178e53981f596a01fa3
shisong02 <shisong02@meituan.com>
2022-05-29 21:34:28 +0800

    bag

--- src/main/java/com/mk/algorithm/solution/dp02/Bag01.java
+++ src/main/java/com/mk/algorithm/solution/dp02/Bag01.java
@@ -23,48 +23,37 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class Bag01 {
-
-    public static int solve(int[] w, int[] v, int N, int W) {
+    public static int bag(int[] weight, int[] value, int w) {
+        int n = value.length;
         // 创建备忘录
-        int[][] dp = new int[N + 1][W + 1];
+        int[][] dp = new int[n+1][w+1];
+
         // 初始化状态
-        for (int i = 0; i <= N; i++) {
-            dp[i][0] = 0;
-        }
-        for (int j = 0; j <= W; j++) {
-            dp[0][j] = 0;
-        }
-        /*
-         * 状态参数
-         * tn: 已经遍历过的物品；
-         * rw: 背包还能容量的重量。
-         */
-        // 遍历每一个物品
-        for (int tn = 1; tn <= N; tn++) {
-            // 背包容量有多大就还要计算多少次
-            for (int rw = 1; rw <= W; rw++) {
-                if (rw < w[tn]) {
+        for (int i = 0; i <= n; i++) { dp[i][0] = 0; }
+        for (int j = 0; j <= w; j++) { dp[0][j] = 0; }
+
+        // 前i件物品
+        for (int i = 1; i <= n; i++) {
+            // 背包容量
+            for (int j = 1; j <= w; j++) {
+                if (j < weight[i-1]) {
                     // 剩余容量不足，只能放下前 tn-1 件物品
-                    dp[tn][rw] = dp[tn - 1][rw];
+                    dp[i][j] = dp[i-1][j];
                 } else {
-                    // 剩余容量充足，决策是否放（状态转移方程）
-                    dp[tn][rw] = Math.max(dp[tn - 1][rw], dp[tn - 1][rw - w[tn]] + v[tn]);
+                    // 剩余容量充足，进一步作出决策（状态转移方程）
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-weight[i-1]] + value[i-1]);
                 }
             }
         }
-
-        return dp[N][W];
+        return dp[n][w];
     }
 
     public static void main(String[] args) {
-        // 物品的总数，背包能容纳的总重量
-        int N = 3, W = 5;
-        // 物品的重量
-        int[] w = {0, 3, 2, 1};
-        // 物品的价值
-        int[] v = {0, 5, 2, 3};
+        int w = 5; // 背包能容纳的总重量
+        int[] weight = {3, 2, 1}; // 物品的重量
+        int[] value = {5, 2, 3}; // 物品的价值
         // 计算结果
-        int result = solve(w, v, N, W);
+        int result = bag(weight, value, w);
         log.info("最大能装价值:" + result);
     }
 }
--- src/main/java/com/mk/algorithm/solution/dp03/Stone.java
+++ src/main/java/com/mk/algorithm/solution/dp03/Stone.java
@@ -2,6 +2,7 @@ package com.mk.algorithm.solution.dp03;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
